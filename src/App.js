52e0fb6cc41fb40fdafc4a7ded9efe4aba9a6b56
Lydia Lind <lydia.nilsson@gmail.com>
2020-06-01 10:46:39 +0200

    move folders to right place

--- src/App.js
+++ src/App.js
@@ -1,10 +1,14 @@
 import React from 'react';
 import './App.css';
+import Button from '../Components/Button';
+import Card from '../Components/Card';
 
 function App() {
   return (
     <div id='app-main'>
       <h1>Hello world</h1>
+      <Button text="Lydia"/>
+      <Card/>
     </div>
   );
 }
