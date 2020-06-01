fcfd82a2118a06fb1e96bb372fb357b4c8c7545f
Lydia Lind <lydia.nilsson@gmail.com>
2020-06-01 10:42:22 +0200

    basic structure for Card component

---
+++ Components/Card.js
@@ -0,0 +1,16 @@
+import React from 'react';
+
+const Card = ({picture, name, popularity, action}) => {
+    return (
+        <table>
+            <thead>
+                <tr>
+                    <th>Picture</th>
+                    <th>Name</th>
+                    <th>Popularity</th>
+                    <th>Action</th>
+                </tr>
+            </thead>
+        </table>
+    )
+}
